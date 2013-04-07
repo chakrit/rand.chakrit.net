edbb00258d9244405075eedd6cd647ea9dfb240b
Chakrit Wichian <chakrit@oozou.com>
2013-04-07 23:08:43 +0700

    Force creation of PID file.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http       = require('http')
   , npid       = require('npid')
   , RandStream = require('randstream');
 
-try { npid.create(__dirname + '/rand.chakrit.net.pid'); }
+try { npid.create(__dirname + '/rand.chakrit.net.pid', true); }
 catch (e) { console.error(e); process.exit(1); }
 
 var server = http.createServer(function(req, resp) {
